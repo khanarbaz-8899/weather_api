7239ae3433d1924f05a4523a5b7777a129eef1c9
khanarbaz-8899 <arbaz70425@gmail.com>
2025-09-15 17:41:49 +0530

    new changed

--- atmos-weather/src/utils/api.js
+++ atmos-weather/src/utils/api.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = process.env.NODE_ENV === 'production' 
+  ? "https://render-express-deployment-1-ep2b.onrender.com/api"  // Your actual Render URL
+  : "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
