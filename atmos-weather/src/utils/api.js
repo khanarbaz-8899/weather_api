e1fae049ab4f8aa1c573c4f4f9392b64bc6f8e1f
khanarbaz-8899 <arbaz70425@gmail.com>
2025-09-15 16:09:06 +0530

    latest

--- atmos-weather/src/utils/api.js
+++ atmos-weather/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:5000/api";
+const API_BASE = "https://your-backend.onrender.com/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
