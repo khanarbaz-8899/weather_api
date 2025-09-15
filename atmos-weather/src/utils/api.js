54d9db0ffae39616b4e05a02d9de3ea36e55e683
khanarbaz-8899 <arbaz70425@gmail.com>
2025-09-15 15:15:32 +0530

    in progress

--- atmos-weather/src/utils/api.js
+++ atmos-weather/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "https://render-express-deployment-d44u.onrender.com/api";
+const API_BASE = "http://localhost:5000/api";
 
 export const api = axios.create({
   baseURL: API_BASE,
